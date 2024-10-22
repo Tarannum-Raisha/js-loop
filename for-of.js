28383f48efb36475db84ab5bd877a736ae3cad22
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-22 22:36:27 +0600

    first commit

---
+++ for-of.js
@@ -0,0 +1,14 @@
+const numbers = [12, 34, 45, 36, 12, 34, 45, 36];
+console.log(numbers);
+
+
+for(const num of numbers){
+    console.log(num);
+}
+
+const fruits = ['orrange', 'apple', 'banana', 'jackfruit'];
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+    
+}
---
+++ while.js
@@ -0,0 +1,18 @@
+const numbers = 7;
+if(numbers<10){
+    console.log('its a small number ');
+
+}
+console.log('hello');
+
+
+// while loopingggggg;
+
+let number = 0;
+
+while(number<5){
+    console.log('loopingggg' ,number);
+    // number = number+1
+    // number +=1;
+    number++;
+}
